f06d30e055d2c2d129eda0af839623d15e4365b3
Nikhil-yd02 <nikhilyadav2000.ny@gmail.com>
2022-07-15 00:46:11 +0530

    Updates

---
+++ precache-manifest.b386e7882a36812c3cde2baed2c28a36.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ac1eb84d79351b923865a245871aa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deb7c7763ce85660f7b7",
+    "url": "/static/css/main.5e486d42.chunk.css"
+  },
+  {
+    "revision": "32ced7c491c648c6c694",
+    "url": "/static/js/2.a422d3f2.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/static/js/2.a422d3f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb7c7763ce85660f7b7",
+    "url": "/static/js/main.51b47672.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.51b47672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "4b9dc077a5e83bc17478662d0144dc92",
+    "url": "/static/media/memoriesLogo.4b9dc077.png"
+  },
+  {
+    "revision": "18ebdb6d48b70ab2e72ac3bf9154d8be",
+    "url": "/static/media/memoriesText.18ebdb6d.png"
+  }
+]);
